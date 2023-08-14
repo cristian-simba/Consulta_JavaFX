aa13458f50ebc93e04db30234bd3b4eabccb18a4
DennisCatana <dennis.joel235@outlook.es>
2023-08-14 10:56:06 -0500

    registro y borrar

--- src/main/java/com/example/consulta_javafx/HelloController.java
+++ src/main/java/com/example/consulta_javafx/HelloController.java
@@ -1,8 +1,13 @@
 package com.example.consulta_javafx;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class HelloController {
     @FXML
@@ -13,6 +18,10 @@ public class HelloController {
     private TextField carreraField;
     @FXML
     private TextField notaField;
+    static final String DB_URL="jdbc:mysql://localhost/Crud";
+    static final String USER="root";
+    static final String PASS="root_bas3";
+    static final String QUERY="Select * From notas ;";
 
     @FXML
     private void registrarBt(ActionEvent event) {
@@ -21,10 +30,33 @@ public class HelloController {
         String carrera = carreraField.getText();
         String nota = notaField.getText();
 
-        System.out.println("se guardo el código: "+codigo);
-        System.out.println("se guardo el nombre: "+nombre);
-        System.out.println("se guardo la carrera: "+carrera);
-        System.out.println("se guardo la nota: "+nota);
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO notas (Codigo, Nombre, Carrera,NotaF) VALUES (?, ?, ?, ?)");
+
+            pstmt.setInt(1, Integer.parseInt(codigo));
+            pstmt.setString(2, nombre);
+            pstmt.setString(3, carrera);
+            pstmt.setInt(4, Integer.parseInt(nota));
+
+            pstmt.executeUpdate();
+
+            showAlert("Registro guardado correctamente.", Alert.AlertType.INFORMATION);
+
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showAlert("Error al guardar el registro.", Alert.AlertType.ERROR);
+        }
+    }
+
+    private void showAlert(String message, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle("Mensaje");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     public void buscarBt(ActionEvent event) {
@@ -36,6 +68,28 @@ public class HelloController {
     }
 
     public void borrarBt(ActionEvent event) {
-        System.out.println("Sirve el Borrar");
+        String codigo = codigoField.getText();
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM notas WHERE codigo = ?");
+
+            pstmt.setInt(1, Integer.parseInt(codigo));
+
+            int rowsAffected = pstmt.executeUpdate();
+
+            if (rowsAffected > 0) {
+                showAlert("Registro eliminado correctamente.", Alert.AlertType.INFORMATION);
+            } else {
+                showAlert("No se encontró ningún registro con el código proporcionado.", Alert.AlertType.WARNING);
+            }
+
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showAlert("Error al eliminar el registro.", Alert.AlertType.ERROR);
+        }
     }
-}
+
+    }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module com.example.consulta_javafx {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
     opens com.example.consulta_javafx to javafx.fxml;
     exports com.example.consulta_javafx;
 }
